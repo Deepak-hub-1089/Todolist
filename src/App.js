0184e00af941c716c6f59a360130dd246551dd45
Deepak-hub-1089 <dee02.acclogin@gmail.com>
2024-06-16 18:34:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "./styles.css";
+import Todo from "./Todo";
+import Dev from "./Dev";
+
+export default function App() {
+  return <Dev />;
+}
---
+++ src/Dev.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Dev() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") {
+      alert("Please enter a task");
+      return;
+    }
+
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dev;
